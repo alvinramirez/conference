dc370406e25189cbfda76bf6ca9564edce4975b7
Alvin Ramirez <alvin.ramirez48@gmail.com>
2024-05-08 16:06:09 +0200

    Editing this postmapping method for validating errors

--- src/main/java/com/example/conference/controller/RegistrationController.java
+++ src/main/java/com/example/conference/controller/RegistrationController.java
@@ -1,7 +1,9 @@
 package com.example.conference.controller;
 
 import com.example.conference.model.Registration;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,8 +20,15 @@ public class RegistrationController {
     }
 
     @PostMapping("registration")
-    public String addRegistration(@ModelAttribute ("registration") Registration registration) {
+    public String addRegistration(@Valid @ModelAttribute ("registration") Registration registration,
+                                  BindingResult result) {
+
+        if (result.hasErrors()) {
+            System.out.println("There were errors");
+            return "registration";
+        }
+
         System.out.println("Registration: " + registration.getName());
-        return "registration";
+        return "redirect:registration";
     }
 }
